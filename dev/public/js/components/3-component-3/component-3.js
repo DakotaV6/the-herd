fd5f626ca84d9b813e5bc506318a3952e11f047c
Hart <hwatnick@umich.edu>
2018-12-04 16:25:23 -0500

    cursor pointer, button ng-show + animation

--- dev/public/js/components/3-component-3/component-3.js
+++ dev/public/js/components/3-component-3/component-3.js
@@ -4,6 +4,10 @@ const component3 = {
     templateUrl: "js/components/3-component-3/component-3.html",
     controller: ["$location", function($location) {
         const vm = this;
+        vm.btnDisplay = false;
+        vm.showBtn = () => {
+            vm.btnDisplay = true;
+        };
         vm.nextUp = () => {
             $location.path("/component-4");
         };

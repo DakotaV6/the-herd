96a8e79828d4da23011638acd39d7322d826d05f
Dakota <dakota.vradenburg@gmail.com>
2018-12-06 11:40:14 -0500

    Playlist fetch now works. Changed hide-options for cleaner directive.

--- dev/public/js/directive/hide-options.js
+++ dev/public/js/directive/hide-options.js
@@ -7,14 +7,14 @@ function hideOptions() {
             $element
             .on("click", (event) => {
                 console.log($element);
-                const options = event.target.attributes[3].value;
-                const icon = event.target.parentElement.children;
+                const selectedOption = event.target.src;
+                const allOptions = event.target.parentElement.children;
                 let i;
-                for (i = 0; i < icon.length; i++) {
-                    if (icon[i].attributes[3].value === options) {
-                        icon[i].hidden = false;
+                for (i = 0; i < allOptions.length; i++) {
+                    if (allOptions[i].src === selectedOption) {
+                        allOptions[i].hidden = false;
                     } else {
-                    icon[i].hidden = true;
+                    allOptions[i].hidden = true;
                     }
                 }
             });

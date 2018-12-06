46c8b0ae5427f296bebf365c580f9c739c299775
Dakota <dakota.vradenburg@gmail.com>
2018-12-06 17:01:45 -0500

    Reset to master

--- server.js
+++ server.js
@@ -3,11 +3,10 @@
 const express = require("express");
 const app = express();
 const routes = require("./spotify.js");
-const port = process.env.PORT || 5000;
 
 app.use(express.static("./dev/public"))
 app.use(express.json());
 app.use("/", routes);
 
 
-app.listen(port, _ => console.log("The server is running."));
+app.listen(3000, _ => console.log("The server is running."));

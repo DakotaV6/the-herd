da5cd2965096edb2c572a6093a72aeff21564f2a
Dakota Vradenburg <41351006+DakotaV6@users.noreply.github.com>
2018-12-07 09:19:09 -0500

    Update shapes.js

--- dev/public/js/components/4-shapes/shapes.js
+++ dev/public/js/components/4-shapes/shapes.js
@@ -25,12 +25,6 @@ const shapes = {
                 easing: 'easeInOutQuad'
             });
         };
-        vm.toPoly = () => {
-            anime({
-                targets: 'polygon',
-                points: '64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96'
-            });
-        };
         vm.toPolygon = () => {
             anime({
             targets: '.star',

0993ccb093e57a0ccbc266b7c453e0312a82bd2d
rachel <rachel.balanon@gmail.com>
2018-12-07 09:04:12 -0500

    added shapes

---
+++ dev/public/js/directive/hide-shape.js
@@ -0,0 +1,32 @@
+"use strict";
+
+function hideShape($timeout) {
+    return {
+        restrict: "A",  
+        link: function($scope, $element, $attrs) {
+            $timeout(function() {
+                $scope.$apply(function() {
+                    $element
+                    .on("click", (event) => {
+                        console.log($element);
+                        const selectedOption = event.target.attributes[1].value;
+                        console.log(selectedOption);
+                        const allOptions = event.target.parentElement.children;
+                        let i;
+                        for (i = 0; i < allOptions.length; i++) {
+                            if (allOptions[i].src === selectedOption) {
+                                allOptions[i].hidden = false;
+                            } else {
+                            allOptions[i].hidden = true;
+                            }
+                        }
+                })
+            
+            })}, 1500);
+        }
+    };
+}
+
+angular
+.module("App")
+.directive("hideShape", hideShape);

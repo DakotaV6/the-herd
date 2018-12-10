f9232654eafe28a058ce035581d76642e04f9e48
rachel <rachel.balanon@gmail.com>
2018-12-10 15:15:44 -0500

    working on draggable directive

--- dev/public/js/directive/draggable.js
+++ dev/public/js/directive/draggable.js
@@ -3,20 +3,41 @@
 function draggable($timeout) {
     return {
         restrict: "A",
-        link: function($scope, $element, $attrs) {
-            $timeout(function() {
-                $scope.$apply(function(){
-                $element
-                    .on("mousedown", (event) => {
-                       
-
+        link: function ($scope, $element, $attrs) {
+            var startX = 0;
+            var startY = 0;
+            var endX = 0;
+            var endY = 0;
+            $timeout(function () {
+                $scope.$apply(function () {
+                    $element
+                        .on("mousedown", (event) => {
+                            event.preventDefault();
+                            event.target.draggable = true;
+                            var startX = event.screenX;
+                            var startY = event.screenY;
+                            console.log(startX, startY);
+                            return startX, startY;
+                        })
+                        
+                        .on("mouseup", (event) => {
+                            event.target.draggable = false;
+                            var endX = event.screenX;
+                            var endY = event.screenY;
+                            console.log(endX, endY);
+                            var distanceTraveled = startY - endY;
+                            console.log(distanceTraveled);
+                            if (distanceTraveled >= 300) {
+                                console.log("dakota has a great metabolism");
+                            }
+                        })
                 })
-            })}, 1600);
-            
+            }, 1600);
+
         }
     };
 }
 
 angular
-.module("App")
-.directive("draggable", draggable);
+    .module("App")
+    .directive("draggable", draggable);
